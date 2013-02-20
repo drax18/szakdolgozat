7066e34280ea00169c4c3f0e81d23213be211751
Drax <Drax@192.168.1.110>
2013-02-20 12:49:01 +0100

    xD

--- application/views/alcohol/sorted2.php
+++ application/views/alcohol/sorted2.php
@@ -94,7 +94,7 @@
            <input class="drinkscount" type="text" value="1" name="db" />Darab
            <div class="cartbutton" ><a class="add_to_cart" href="<?php echo $row->link_name; ?>"><img src="<?php echo base_url()."/img/cartbutton.png" ;?>" /></a></div>
            <div class="informbuttom"><a href="<?php echo site_url("alcohol/drink/$row->link_name"); ?>"><img src="<?php echo base_url()."/img/moreinform.png" ;?>" /><a/></div>
-           <a class="favoradd"  href="" data-linkname="<?php echo $row->link_name; ?>">Kedvencekhez +</a>
+           <a class="favoradd"  href="" data-linkname="<?php echo $row->link_name; ?>"><img src="<?php echo base_url("img/favadd.png") ?>" /></a>
            <div class="alreadyfav" data-<?php echo $row->link_name; ?>="1">
                           <?php
                           if($this->session->userdata('username')){
