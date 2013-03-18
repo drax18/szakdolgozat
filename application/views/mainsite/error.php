990704807ac0f74510588e0842416ca6ae804567
Drax <Drax@192.168.1.110>
2013-03-18 11:27:39 +0100

    xD

--- application/views/mainsite/error.php
+++ application/views/mainsite/error.php
@@ -1,5 +1,8 @@
 <div class="error">
     <?php
-echo "Az oldal csak FIREFOX, CHROME és OPERA böngészőkön elérhetők! Kérlek töltsd le ezek valamelyikét !";
+echo "Az oldal csak FIREFOX, CHROME és OPERA böngészőkön elérhetők! Kérlek töltsd le ezek valamelyikét !".br(1);
 ?>
+    <p>Google Chrome:<a href="">link</a></p>
+    <p>Firefox:<a href="">link</a></p>
+    <p>Opera:<a href="">link</a></p>
 </div>
